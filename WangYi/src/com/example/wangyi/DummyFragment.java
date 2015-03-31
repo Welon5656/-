d7e7a545ee2de7896ad6174610c6f2b732b8c235
Welon5656 <13728884480@139.com>
2015-03-31 22:56:05 +0800

    WangYi

--- WangYi/src/com/example/wangyi/DummyFragment.java
+++ WangYi/src/com/example/wangyi/DummyFragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 import android.widget.TextView;
 
 public class DummyFragment extends Fragment 
@@ -16,10 +17,12 @@ public class DummyFragment extends Fragment
 			Bundle savedInstanceState) {
 		View fragmentView = inflater.inflate(R.layout.fragment, container, false);
 		TextView textView = (TextView) fragmentView.findViewById(R.id.textView);
+		WebView webview = (WebView) fragmentView.findViewById(R.id.webView);
 		//获取Activity传递过来的参数
 		Bundle mBundle = getArguments();
 		String title = mBundle.getString(Title);
 		textView.setText(title);
+		webview.loadUrl("http://news.163.com/");
 		//返回View
 		return fragmentView;
 	}
