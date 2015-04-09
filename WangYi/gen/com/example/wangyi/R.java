810deae6feeccd1c3292e42c49ec02a014a82d23
Welon5656 <13728884480@139.com>
2015-04-09 17:27:06 +0800

    WangYi

--- WangYi/gen/com/example/wangyi/R.java
+++ WangYi/gen/com/example/wangyi/R.java
@@ -28,14 +28,30 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090003;
-        public static final int pager=0x7f090000;
-        public static final int textView=0x7f090001;
-        public static final int webView=0x7f090002;
+        public static final int action_settings=0x7f090011;
+        public static final int button1=0x7f09000b;
+        public static final int button2=0x7f09000c;
+        public static final int button3=0x7f09000d;
+        public static final int drawer_layout=0x7f090000;
+        public static final int left_drawer=0x7f090002;
+        public static final int leftlinear=0x7f090006;
+        public static final int menu_log_in=0x7f09000e;
+        public static final int news=0x7f090007;
+        public static final int pager=0x7f090001;
+        public static final int photo=0x7f090009;
+        public static final int right_drawer=0x7f090003;
+        public static final int scanner=0x7f090010;
+        public static final int subscribe=0x7f090008;
+        public static final int textView=0x7f090004;
+        public static final int video=0x7f09000a;
+        public static final int weather=0x7f09000f;
+        public static final int webView=0x7f090005;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int fragment=0x7f030001;
+        public static final int leftlinear=0x7f030002;
+        public static final int rightlinear=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f080000;
@@ -43,7 +59,15 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f060002;
         public static final int app_name=0x7f060000;
+        public static final int drawer_close=0x7f060004;
+        public static final int drawer_open=0x7f060003;
         public static final int hello_world=0x7f060001;
+        public static final int news=0x7f060007;
+        public static final int photo=0x7f060009;
+        public static final int scanner=0x7f060006;
+        public static final int subscribe=0x7f060008;
+        public static final int video=0x7f06000a;
+        public static final int weather=0x7f060005;
     }
     public static final class style {
         /** 
--- WangYi/src/com/example/wangyi/MainActivity.java
+++ WangYi/src/com/example/wangyi/MainActivity.java
@@ -1,24 +1,25 @@
 package com.example.wangyi;
 
+import java.lang.reflect.Field;
+
 import android.app.ActionBar;
+import android.app.Activity;
 import android.app.FragmentTransaction;
+import android.content.Context;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewConfiguration;
+import android.widget.LinearLayout;
 
-/**
- * Description: <br/>
- * website: <a href="http://www.crazyit.org">crazyit.org</a> <br/>
- * Copyright (C), 2001-2014, Yeeku.H.Lee <br/>
- * This program is protected by copyright laws. <br/>
- * Program Name: <br/>
- * Date:
- * 
- * @author Yeeku.H.Lee kongyeeku@163.com
- * @version 1.0
- */
 public class MainActivity extends FragmentActivity
 	implements ActionBar.TabListener {
 	
@@ -26,20 +27,26 @@ public class MainActivity extends FragmentActivity
 	private ViewPager viewPager;
 	private ActionBar actionBar;
 	private FragmentPagerAdapter pagerAdapter;
+	private DrawerLayout mDrawerLayout;
+	private ActionBarDrawerToggle mDrawerToggle;
+	private LinearLayout leftView,rightView;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		// TabTitles存储每个项的标题
+		getOverflowMenu();
+		init();
+	}
+	
+	private void init() {
+		/* TabTitles存储每个项的标题 */
 		TabTitles = getResources().getStringArray(R.array.tab_title);
-		// 获取ActionBar对象
 		actionBar = getActionBar();
-		// 获取ViewPager
 		viewPager = (ViewPager) findViewById(R.id.pager);
-		// 创建一个FragmentPagerAdapter对象，该对象负责为ViewPager提供多个Fragment
+		/* 创建一个FragmentPagerAdapter对象，该对象负责为ViewPager提供多个Fragment */
 		pagerAdapter = new FragmentPagerAdapter(getSupportFragmentManager()) {
-			// 获取第position位置的Fragment
+			/* 获取第position位置的Fragment */
 			@Override
 			public Fragment getItem(int position) {
 				Fragment fragment = new DummyFragment();
@@ -48,23 +55,26 @@ public class MainActivity extends FragmentActivity
 				fragment.setArguments(args);
 				return fragment;
 			}
-			// 该方法的返回值i表明该Adapter总共包括多少个Fragment
+			/* 该方法的返回值表明该Adapter总共包括多少个Fragment */
 			@Override
 			public int getCount() {
 				return TabTitles.length;
 			}
 		};
-		// 设置ActionBar使用Tab导航方式
+		/* 设置ActionBar使用Tab导航方式 */
 		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-		//为ActionBar添加Tab并设置TabListener
+		/* 为ActionBar添加Tab并设置TabListener */
 		for(int i=0; i<TabTitles.length; i++) {
 			ActionBar.Tab tab = actionBar.newTab();
 			tab.setText(TabTitles[i]);
 			tab.setTabListener(this);
 			actionBar.addTab(tab, i);
 		}
+		actionBar.setHomeButtonEnabled(true);      // 
+		actionBar.setDisplayHomeAsUpEnabled(true); //
+		actionBar.setDisplayShowHomeEnabled(true); //
 		// 为ViewPager组件设置FragmentPagerAdapter
-		viewPager.setAdapter(pagerAdapter);  //①
+		viewPager.setAdapter(pagerAdapter);
 		// 为ViewPager组件绑定事件监听器
 		viewPager.setOnPageChangeListener(
 				new ViewPager.SimpleOnPageChangeListener() {
@@ -72,9 +82,129 @@ public class MainActivity extends FragmentActivity
 					@Override
 					public void onPageSelected(int position) {
 						actionBar.setSelectedNavigationItem(position);
-					}	
+					}
 				});
+		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+		mDrawerToggle = new ActionBarDrawerToggle(
+                this,                   /* host Activity */
+                mDrawerLayout,          /* DrawerLayout object */
+                R.drawable.ic_launcher, /* nav drawer image to replace 'Up' caret */
+                R.string.drawer_open,   /* "open drawer" description for accessibility */
+                R.string.drawer_close   /* "close drawer" description for accessibility */
+                ) {
+			/* openDrawer、closeDrawer等方法可以在此重写 */
+        };
+
+        // 为ActionBarDrawerToggle对象设置监听，还添加了相关方法如openDrawer、closeDrawer等
+        mDrawerLayout.setDrawerListener(new MyDrawerListener());
+        leftView = (LinearLayout) findViewById(R.id.left_drawer);
+        rightView = (LinearLayout) findViewById(R.id.right_drawer);
+        MenuLandscapeLinearLayout LeftLinearLayout = new MenuLandscapeLinearLayout(this, R.layout.leftlinear);  
+        MenuLandscapeLinearLayout RightLinearLayout = new MenuLandscapeLinearLayout(this, R.layout.rightlinear);
+        leftView.addView(LeftLinearLayout);
+        rightView.addView(RightLinearLayout);
+	}
+
+	/* ActionBar不够空间显示和不显示的item将会放置在OverflowMenu中 */
+	private void getOverflowMenu() {
+		ViewConfiguration viewConfig = ViewConfiguration.get(this);
+		
+		try {
+			Field overflowMenuField = ViewConfiguration.class.getDeclaredField("sHasPermanentMenuKey");
+			if(null != overflowMenuField) {
+				overflowMenuField.setAccessible(true);
+				overflowMenuField.setBoolean(viewConfig, false);
+			}
 		}
+		catch (NoSuchFieldException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	/* 监听DrawerLayout的各个状态 */
+	private class MyDrawerListener implements DrawerLayout.DrawerListener {
+
+		@Override
+		public void onDrawerClosed(View arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void onDrawerOpened(View arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void onDrawerSlide(View arg0, float arg1) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void onDrawerStateChanged(int arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+	
+	/* 右边栏的相关方法 */
+	public void openRightLayout() {
+		if (mDrawerLayout.isDrawerOpen(leftView))
+			mDrawerLayout.closeDrawer(leftView);
+		else if (mDrawerLayout.isDrawerOpen(rightView))
+			mDrawerLayout.closeDrawer(rightView);
+		else
+			mDrawerLayout.openDrawer(rightView);
+	}
+	
+	public class MenuLandscapeLinearLayout extends LinearLayout {
+		/* 构造函数 */
+	    public MenuLandscapeLinearLayout(Context context, int res) {
+	    	super(context);
+	    	// TODO Auto-generated constructor stub
+	    	//加载需要的属性，加载子Layout
+	    	((Activity) getContext()).getLayoutInflater().inflate(res, this); 
+	    }	
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	@Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+		if (mDrawerLayout.isDrawerOpen(rightView)) {
+			mDrawerLayout.closeDrawer(rightView);
+		}
+		else {
+			mDrawerToggle.onOptionsItemSelected(item);
+		}
+        switch(item.getItemId()) {
+		case R.id.menu_log_in:
+			System.out.print("ok");
+			openRightLayout();
+			break;
+		case R.id.action_settings:
+			System.out.print("ok");
+			//finish();
+			break;
+		}
+        return super.onOptionsItemSelected(item);
+    }
 	
 	@Override
 	public void onTabUnselected(ActionBar.Tab tab,
@@ -82,7 +212,7 @@ public class MainActivity extends FragmentActivity
 		
 	}
 
-	// 当指定Tab被选中时激发该方法
+	/* 当指定Tab被选中时激发该方法 */
 	@Override
 	public void onTabSelected(ActionBar.Tab tab,
 			FragmentTransaction fragmentTransaction) {
@@ -94,4 +224,20 @@ public class MainActivity extends FragmentActivity
 			FragmentTransaction fragmentTransaction) {
 		
 	}
+	
+	@Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        /* syncState()会自动和actionBar关联, 将开关的图片显示在了action上，如果不设置，也可以有抽屉的效果，不过是默认的图标 */
+        mDrawerToggle.syncState();
+    }
+	
+	/* 设备配置改变时 */
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggls
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+		
 }
